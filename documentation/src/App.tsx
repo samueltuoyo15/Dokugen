dc195dab27b10f3b33a772baf2cb7ad910b8d9cb
Adebayo Muis <charmingdc002@gmail.com>
2025-02-19 22:21:11 +0100

    Created tthe introduction page and setup routing

--- documentation/src/App.tsx
+++ documentation/src/App.tsx
@@ -1,21 +1,31 @@
-// import { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
-import Home from './pages/Home.tsx';
-import Introduction from './pages/Introduction.tsx';
-import Installation from './pages/Installation.tsx';
+import Layout from './components/Layout.tsx';
+const Home = lazy(() => import('./pages/Home.tsx'));
+const Installation = lazy(() => import('./pages/Installation.tsx'));
+
 
 import './App.css'
 
 const App = () => {
  return (
    <BrowserRouter>
-     <Routes>
-       <Route path='/' element={<Home />} />
-       <Route path='/intro' element={<Introduction />} />
-       <Route path='/installation' element={<Installation />}/>
-     </Routes>
+    <Routes>
+     <Route path='/' element={<Layout />}>
+       <Route index element={
+        <Suspense>
+         <Home />
+        </Suspense>
+       } />
+       <Route path='installation' element={
+        <Suspense>
+          <Installation />
+        </Suspense>
+       } />
+     </Route>
+    </Routes>
    </BrowserRouter>
  )
 }
