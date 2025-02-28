2b4c7ad3ff95cf228a70dd47dfc5c3b3b8d4d667
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-28 14:09:57 +0100

    code refractor

--- dashboard/lib/supabase.ts
+++ dashboard/lib/supabase.ts
@@ -3,4 +3,4 @@ import dotenv from "dotenv"
 dotenv.config()
 
 
-export const supabase = createClient(process.env.SUPABASE_CLIENT_URL.trim() || "", process.env.SUPABASE_ANON_KEY.trim() || "")
+export const supabase = createClient(process.env.SUPABASE_CLIENT_URL!.trim(), process.env.SUPABASE_ANON_KEY!.trim())
