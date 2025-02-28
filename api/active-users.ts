10e8e58d804a1f3748b5087c94443dfc7206df9e
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-28 02:11:01 +0100

    fixed erros and added active users count

---
+++ api/active-users.ts
@@ -0,0 +1,18 @@
+import supabase from "../lib/supabase.mjs"
+import { VercelRequest, VercelResponse } from "@vercel/node"
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  if(req.method !== "GET"){
+    return res.status(405).json({message: "Method not allowed "})
+  }
+  
+  try{
+    const {data, error } = await supabase.from("active_users").select("id, email, username, usage_count").order("usage_count", {ascending: false}).limit(20)
+    if(error) throw error
+    
+    return res.status(200).json({activeUsers: data})
+  } catch(error: any){
+    console.error("Error Fetching Active Users", error)
+    return res.status(500).json({message: "Failed to fetch active users "})
+  }
+}
