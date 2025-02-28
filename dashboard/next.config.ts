cd73adeeef52133be9629a54affd74a074e5387d
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-28 15:56:16 +0100

    code refractor fixed

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,10 +1,7 @@
-import type { NextConfig } from "next"
-//import os from "os"
-
-//const isLinuxArm64 = os.platform() === "linux" && os.arch() === "arm64"
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
