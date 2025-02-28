f0afec3d638dee1af9668491986728c84f8654ea
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-28 15:45:24 +0100

    code refractor

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next"
-import os from "os"
+//import os from "os"
 
 //const isLinuxArm64 = os.platform() === "linux" && os.arch() === "arm64"
 
 const nextConfig: NextConfig = {
-  experimental: {
-    //turbo: true,
-    disablePostcssPresetEnv: true,
-   },
+  
 }
 
 export default nextConfig
