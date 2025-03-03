ae38559f57013c8818bcaf335ac9ba36151513e0
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-03-03 23:34:04 +0100

    fixed

--- dashboard/next-sitemap.config.ts
+++ dashboard/next-sitemap.config.ts
@@ -1,3 +1,3 @@
 import siteMetadata from "./utils/siteMetaData"
 
-export const config = { siteUrl: siteMetadata.siteUrl, generateRobotsTxt: true, }
+export const config = { siteUrl: siteMetadata.siteUrl || "https://dokugen-readme.vercel.app/", generateRobotsTxt: true, }
