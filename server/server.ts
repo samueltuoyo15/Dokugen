42229392ee8450b417abd5e5e045f4b43f2ed0e9
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-03-26 18:57:04 +0100

    some fixes

--- server/server.ts
+++ server/server.ts
@@ -4,12 +4,16 @@ import crypto from "crypto"
 import os from "os"
 import { v4 as uuidv4 } from "uuid"
 import { OpenAI } from "openai"
+import compression from "compression"
 import dotenv from "dotenv"
 dotenv.config()
 
 const app: Application = express()
-app.use(express.json())
-
+app.use(cors("*"))
+app.use(helmet())
+app.use(compression())
+app.use(express.json({limit: "50mb"}))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || "",
   baseURL: process.env.OPENAI_ENDPOINT || "https://api.openai.com/v1/chat/completions"
